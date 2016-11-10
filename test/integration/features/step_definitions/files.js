0bebae1bcb146cd5421c81238523eb8262c6c517
Matt Travi <programmer@travi.org>
2016-11-10 09:28:37 -0600

    fix instability in comparing generated values for the .npmrc file

--- test/integration/features/step_definitions/files.js
+++ test/integration/features/step_definitions/files.js
@@ -11,7 +11,7 @@ module.exports = function () {
 
   const projectName = any.word();
   const license = any.word();
-  const nodeVersion = any.string();
+  const nodeVersion = any.word();
 
   this.Before(function () {
     this.answerPromptsWith({projectName, license});
