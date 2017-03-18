c77a150f556923259dd46c1c6eb9f0967c1539c9
Matt Travi <programmer@travi.org>
2017-03-18 14:20:27 -0500

    test(generator-git-stub): fixed the git-config stub for the underlying git generator

--- test/integration/features/step_definitions/git.js
+++ test/integration/features/step_definitions/git.js
@@ -1,4 +1,5 @@
-const gitConfig = require('parse-git-config');
+const gitConfig = require('git-config');
+const parseGitConfig = require('parse-git-config');
 const any = require('@travi/any');
 const sinon = require('sinon');
 const defineSupportCode = require('cucumber').defineSupportCode;
@@ -16,15 +17,13 @@ defineSupportCode(({Before, After, setWorldConstructor}) => {
     this.githubRepo = `${user}/${repo}`;
 
     sandbox = sinon.sandbox.create();
-    sandbox.stub(gitConfig, 'sync').returns({
-      user: {name},
+    sandbox.stub(gitConfig, 'sync').returns({user: {name}});
+    sandbox.stub(parseGitConfig, 'sync').returns({
       'remote "origin"': {
-        url: `git@github.com:${user}/${repo}`
+        url: `git@github.com:${this.githubRepo}`
       }
     });
   });
 
-  After(() => {
-    sandbox.restore();
-  });
+  After(() => sandbox.restore());
 });
